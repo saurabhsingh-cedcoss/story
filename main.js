75016d790e788bc2f9392f7b02c4f6f5700bb6ce
saurabhsingh-cedcoss <74813543+saurabhsingh-cedcoss@users.noreply.github.com>
2020-12-12 11:29:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+function consol() {
+      if(localStorage.length==0){
+     localStorage.setItem('animalName', document.getElementById("inpAnimal").value);
+     localStorage.setItem('anotherAnimalName', document.getElementById("inpAnotherAnimal").value);
+     localStorage.setItem('more', document.getElementById("inpMore").value);
+     localStorage.setItem('past', document.getElementById("inpPast").value);
+     localStorage.setItem('quot', document.getElementById("quote").value);
+     localStorage.setItem('msg', document.getElementById("message").value);
+     localStorage.setItem('num', document.getElementById("inpNumber").value);
+     localStorage.setItem('radio', document.querySelector("input[name='radio']:checked").value);
+     localStorage.setItem('speed', document.querySelector("select[name='speed']").value);
+      }
+    }
+   
+    function story(){
+        if(localStorage.length>0){
+    var sanimal = document.querySelectorAll(".sanimal");
+    for (i = 0; i < sanimal.length; i++) {
+        sanimal[i].innerHTML = localStorage.getItem('animalName');
+    }
+    var sAnotherAnimal = document.querySelectorAll(".sAnotherAnimal");
+    for (i = 0; i < sAnotherAnimal.length; i++) {
+        sAnotherAnimal[i].innerHTML = localStorage.getItem('anotherAnimalName');
+    }
+    var sverb = document.querySelectorAll(".sverb");
+    for (i = 0; i < sverb.length; i++) {
+        sverb[i].innerHTML = localStorage.getItem('verb');
+    }
+    
+
+
+     document.getElementById("sradio").innerHTML = localStorage.getItem("radio");
+     document.getElementById("smore").innerHTML = localStorage.getItem("more");
+     document.getElementById("smessage").innerHTML = localStorage.getItem('msg');
+     document.getElementById("snum").innerHTML = localStorage.getItem('num');
+     document.getElementById("sspeed").innerHTML = localStorage.getItem('speed');
+     document.getElementById("spast").innerHTML = localStorage.getItem('past');
+     document.getElementById("sQuote").innerHTML = localStorage.getItem('quot');
+     localStorage.clear();}
+
+}
+
